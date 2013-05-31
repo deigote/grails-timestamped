7bd41cba8e7fe82c5dff0c3fc5d9ae30d92bd1f0
deigote <diego@toharia.com>
2013-05-31 16:01:39 +0200

    Utility script to test the plugin

---
+++ test-plugin.sh
@@ -0,0 +1,6 @@
+git submodule update --init && \
+grails package-plugin && \
+cp target/grails-plugin-timestamped-*.jar TimestampedDemo/lib/ && \
+cd TimestampedDemo/ && \
+./test.sh 
+git checkout -- lib/grails-plugin-timestamped-*.jar 
