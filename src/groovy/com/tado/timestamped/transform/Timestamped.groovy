8509a58dde142f3060fd4266baf4eca82ee18c9f
Burt Beckwith <burt@burtbeckwith.com>
2013-05-31 09:58:24 -0400

    cleanup

--- src/groovy/com/tado/timestamped/transform/Timestamped.groovy
+++ src/groovy/com/tado/timestamped/transform/Timestamped.groovy
@@ -2,10 +2,9 @@ package com.tado.timestamped.transform
 
 import java.lang.annotation.ElementType
 import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.RetentionPolicy
 import java.lang.annotation.Target
 import org.codehaus.groovy.transform.GroovyASTTransformationClass
-
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target([ ElementType.TYPE ])
